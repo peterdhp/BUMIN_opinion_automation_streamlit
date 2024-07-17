72d665e3ea5375ba43c9da307bf265ac6c200c4f
Dohoon <peterdhp0205@gmail.com>
2024-07-17 15:18:28 +0900

    feedback update for full_demo

--- full_demo.py
+++ full_demo.py
@@ -2,11 +2,13 @@ import streamlit as st
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_openai import ChatOpenAI
+from streamlit_feedback import streamlit_feedback
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain.output_parsers.openai_tools import JsonOutputKeyToolsParser
 from typing import List
 from langchain_core.documents import Document
 from langchain_core.output_parsers import StrOutputParser, CommaSeparatedListOutputParser
+from langchain_core.tracers.context import collect_runs
 from langchain_core.runnables import (
     RunnableLambda,
     RunnableParallel,
@@ -19,6 +21,7 @@ from langchain.vectorstores import FAISS
 import cohere
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langsmith import traceable
+from langsmith import Client
 
 
 os.environ["LANGCHAIN_API_KEY"]=st.secrets['LANGCHAIN_API_KEY']
@@ -30,6 +33,7 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key', value = '',type='passwo
 if openai_api_key =='bumin':
     openai_api_key = st.secrets['OPENAI_API_KEY']
 
+client = Client()
 
 #openai_api_key = os.environ.get('OPENAI_API_KEY')
 
@@ -110,7 +114,7 @@ if "opinion" not in st.session_state:
     
     
 st.title('판정 소견 자동 완성')
-
+feedback_option = "thumbs"
 
 
 with st.form('my_form'):
@@ -119,8 +123,54 @@ with st.form('my_form'):
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
-        response = opinion_generator(model="gpt-4o").invoke(result_report)
+        chain = opinion_generator(model="gpt-4o")
+        with collect_runs() as cb:
+            response = chain.invoke(result_report)
+            st.session_state.run_id = cb.traced_runs[0].id
         opinion = response['opinion']
         doc_list = '\n\n'.join([f"{entry.metadata['test']}  :  {entry.metadata['korean']}" for i, entry in enumerate(response['docs'])])
         st.session_state.opinion = st.info(opinion)
+        if st.session_state.get("run_id"):
+            run_id = st.session_state.run_id
+            feedback = streamlit_feedback(
+                feedback_type=feedback_option,
+                optional_text_label="[Optional] Please provide an explanation",
+                key=f"feedback_{run_id}",
+            )
+
+            # Define score mappings for both "thumbs" and "faces" feedback systems
+            score_mappings = {
+                "thumbs": {"👍": 1, "👎": 0},
+                "faces": {"😀": 1, "🙂": 0.75, "😐": 0.5, "🙁": 0.25, "😞": 0},
+            }
+
+            # Get the score mapping based on the selected feedback option
+            scores = score_mappings[feedback_option]
+
+            if feedback:
+                # Get the score from the selected feedback option's score mapping
+                score = scores.get(feedback["score"])
+
+                if score is not None:
+                    # Formulate feedback type string incorporating the feedback option
+                    # and score value
+                    feedback_type_str = f"{feedback_option} {feedback['score']}"
+
+                    # Record the feedback with the formulated feedback type string
+                    # and optional comment
+                    feedback_record = client.create_feedback(
+                        run_id,
+                        feedback_type_str,
+                        score=score,
+                        comment=feedback.get("text"),
+                    )
+                    st.session_state.feedback = {
+                        "feedback_id": str(feedback_record.id),
+                        "score": score,
+                    }
+                else:
+                    st.warning("Invalid feedback score.")
+        
+        
+        
         st.info(doc_list)
