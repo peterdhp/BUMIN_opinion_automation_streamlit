8f43df91bccb369c91f42d09dfe85663a26f01be
Dohoon <peterdhp0205@gmail.com>
2024-10-25 10:43:38 +0900

    ? delete

--- pages/validation2.py
+++ pages/validation2.py
@@ -36,6 +36,7 @@ if uploaded_file:
         # Load the uploaded Excel file into a DataFrame
         df = pd.read_excel(uploaded_file)
         df = df.replace({'_x000D_\n': '\n'}, regex=True)
+        f = df.replace({'?': ''}, regex=True)
         
         # Filter rows where 'type' is 2
         filtered_df = df[df['type'] == 2]
