6d8c1468ec6674f4d69354159e7d6dec270bd746
Dohoon <peterdhp0205@gmail.com>
2024-10-28 11:11:58 +0900

    promt fix

--- validation_engine.py
+++ validation_engine.py
@@ -49,7 +49,7 @@ class MismatchComment(BaseModel):
     )
 structured_llm_commenter = llm4o.with_structured_output(MismatchComment)
 
-comment_system_prompt = """When given a explanation that does not match the test report, give a comment why it doesn't match.\n\nBe as clear and brief as possible. Maximum of 10 words. Be especially clear about missing biopsy results and unmatching follow-up period. Use Korean"""
+comment_system_prompt = """When given a explanation that does not match the test report, give a comment why it doesn't match.\n\nBe as clear and brief as possible. Maximum of 10 words. Be especially clear about when the test report is missing biopsy results mentioned in the explanation and unmatching follow-up period. Use Korean"""
 comment_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", comment_system_prompt),
