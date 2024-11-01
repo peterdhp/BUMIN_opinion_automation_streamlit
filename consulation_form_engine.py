3c130a2dc92bf7eae2c87fb8b78ab5d2af906d63
Dohoon <peterdhp0205@gmail.com>
2024-11-01 18:10:51 +0900

    again

--- consulation_form_engine.py
+++ consulation_form_engine.py
@@ -108,7 +108,7 @@ def generate(state):
     """
     input = state["input"]
     
-    response = hallucination_grader.invoke({'test_results':input})
+    response = hallucination_grader.invoke({'report':input})
     return {"response": response}
   
     
